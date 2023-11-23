0d64cee71aa6ceae78b80775eae7d3e5b8d65be3
Andre Bagestan <andrebagestan@gmail.com>
2023-11-23 13:42:41 -0300

    Create form structure and style

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './forms.component.html',
-  styleUrl: './forms.component.css'
+  styleUrl: './forms.component.css',
 })
 export class FormsComponent {
-
+  results = [];
 }
